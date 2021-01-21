0c30d897fb487287830ec7414e37d8fe12108d02
Benjamin Geets <45077541+benjamingeets@users.noreply.github.com>
2021-01-21 16:47:48 +0100

    d

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target:'static',
   head: {
     title: 'VisualKraken - Votre site, au pixel près',
     htmlAttrs: {
