26e1191fe1708aba182701b0a917e7d83beb8b55
Benjamin Geets <45077541+benjamingeets@users.noreply.github.com>
2021-01-24 21:35:10 +0100

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form name="contactus" action="/a-propos" method="POST"  netlify netlify-honeypot="bot-field">
+    <form name="contactus" action="/contact?envoie=true" method="POST"  netlify netlify-honeypot="bot-field">
      <input type="hidden" name="form-name" value="contactus" />
             <div id="nommail">
                 <div id="nom">
