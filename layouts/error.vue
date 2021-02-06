4f65ace3a2c5edd5045c8fd8f9298f92757dad2b
Benjamin Geets <bgeets@icloud.com>
2021-02-06 20:54:34 +0100

    ajout de la vraie 404

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,26 @@
 <template>
-    <div>404 oui
-        
+    <div class="error-page">
+        <h2>Erreur 🦑</h2>
+        <p>Cette page n'existe pas</p>
+        <NuxtLink to="/">
+             <div class="button">
+                <p>Retour à l'accueil</p>
+            </div>
+        </NuxtLink>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .error-page{
+        
+        margin:auto;
+        padding-top:100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
 
 <script>
 export default {
