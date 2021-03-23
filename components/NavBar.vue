373be821abc7895a2254b7121785717c6d9c5054
Benjamin Geets <bgeets@icloud.com>
2021-03-23 10:50:16 +0100

    iphone 5 compatibility (nav)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,232 +1,241 @@
 <template>
-    <nav>
-        <div class="logo">
-            <NuxtLink to="/">
-                <img src="~/assets/logo.png" alt="">
-                <h1>VisualKraken</h1>
-            </NuxtLink>
-        </div>
-        <ul>
-            <li><NuxtLink to="/">Accueil</NuxtLink></li>
-            <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-            <li><NuxtLink to="/a-propos">A propos</NuxtLink></li>
-        </ul>
-        <div class="hamburger" @click="hmbMenu()">
-            <img src="~/assets/hamburger.png" alt="">
-        </div>
-        <div class="hamburger-menu" style="margin-left:-100vw" @click="hmbMenu()">
-            <div class="close">
-                <img src="~/assets/cross.png" alt="">
-            </div>
-           <img src="~/assets/logo_white.svg" alt="">
-            <h1>VisualKraken</h1>
-            <p><NuxtLink to="/">Accueil</NuxtLink></p>
-            <p><NuxtLink to="/blog">Blog</NuxtLink></p>
-            <p><NuxtLink to="/contact">Contact</NuxtLink></p>
-            <p><NuxtLink to="/a-propos">A propos</NuxtLink></p>
-        </div>
-    </nav>
+  <nav>
+    <div class="logo">
+      <NuxtLink to="/">
+        <img src="~/assets/logo.png" alt="" />
+        <h1>VisualKraken</h1>
+      </NuxtLink>
+    </div>
+    <ul>
+      <li><NuxtLink to="/">Accueil</NuxtLink></li>
+      <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+      <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+      <li><NuxtLink to="/a-propos">A propos</NuxtLink></li>
+    </ul>
+    <div class="hamburger" @click="hmbMenu()">
+      <img src="~/assets/hamburger.png" alt="" />
+    </div>
+    <div class="hamburger-menu" style="margin-left: -100vw" @click="hmbMenu()">
+      <div class="close">
+        <img src="~/assets/cross.png" alt="" />
+      </div>
+      <img src="~/assets/logo_white.svg" alt="" />
+      <h1>VisualKraken</h1>
+      <p><NuxtLink to="/">Accueil</NuxtLink></p>
+      <p><NuxtLink to="/blog">Blog</NuxtLink></p>
+      <p><NuxtLink to="/contact">Contact</NuxtLink></p>
+      <p><NuxtLink to="/a-propos">A propos</NuxtLink></p>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss">
-$bleu : #2463AA;
-$transition : 0.3s ease-in-out;
+$bleu: #2463aa;
+$transition: 0.3s ease-in-out;
 
-
-
-.stop-scroll{
-    overflow: hidden;
+.stop-scroll {
+  overflow: hidden;
 }
 
-.hamburger-menu{
-    display: none;
+.hamburger-menu {
+  display: none;
 }
 
-nav{
-    height: 15vh;
+nav {
+  height: 15vh;
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: auto;
+  .logo {
+    width: 60%;
+    a {
+      cursor: default;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      img {
+        height: 80%;
+        cursor: pointer;
+      }
+      h1 {
+        color: $bleu;
+        cursor: pointer;
+      }
+    }
+  }
+  ul {
     display: flex;
+    width: 40%;
     justify-content: space-between;
-    width: 80%;
-    margin:auto;
-    .logo{
-        width: 60%;
-        a{  
-            cursor: default;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            img{
-                height: 80%;
-                cursor: pointer;
-            }
-            h1{
-                color:$bleu;
-                cursor: pointer;
-            }
+    align-items: center;
+    li {
+      a {
+        color: black;
+        &:hover {
+          color: $bleu;
+          transition: $transition;
         }
-    }
-    ul{
-        display: flex;
-        width: 40%;
-        justify-content: space-between;
-        align-items: center;
-        li{
-            a{
-                color:black;
-                &:hover{
-                    color:$bleu;
-                    transition: $transition;
-                }
-                &::after{
-                    content: '';
-                    display: block;
-                    width: 0;
-                    height: 2px;
-                    background: $bleu;
-                    transition: width .3s;
-                    margin-top:10px;
-                }
-                &:hover::after{
-                    width: 100%;
-                }
-            }
-            .nuxt-link-exact-active{
-                    color:$bleu;
-                    &::after{
-                        content: '';
-                        display: block;
-                        width: 100%;
-                        height: 2px;
-                        background: $bleu;
-                        transition: width .3s;
-                        margin-top:10px
-                    }
-                }
+        &::after {
+          content: "";
+          display: block;
+          width: 0;
+          height: 2px;
+          background: $bleu;
+          transition: width 0.3s;
+          margin-top: 10px;
         }
+        &:hover::after {
+          width: 100%;
+        }
+      }
+      .nuxt-link-exact-active {
+        color: $bleu;
+        &::after {
+          content: "";
+          display: block;
+          width: 100%;
+          height: 2px;
+          background: $bleu;
+          transition: width 0.3s;
+          margin-top: 10px;
+        }
+      }
     }
-    .hamburger{
-       display: none;
-    }
+  }
+  .hamburger {
+    display: none;
+  }
 }
-@media (max-width:1024px){
-    nav{
-        height: 120px ;
-        .logo{
-            width: 100%;
-            a{
-                margin:auto;
-                img{
-                margin: 0 0 0 auto;
-                }
-                h1{
-                    margin: 0 auto 0 0;
-                }
-            }
+@media (max-width: 1024px) {
+  nav {
+    height: 120px;
+    .logo {
+      width: 100%;
+      a {
+        margin: auto;
+        img {
+          margin: 0 0 0 auto;
         }
-        ul{
-            display: none;
+        h1 {
+          margin: 0 auto 0 0;
         }
-        .hamburger{
-            display: inline;
-            position: absolute;
-            right:60px;
-            top:70px;
-            width: 40px;
-            
-            cursor: pointer;
-            img{
-                width: 100%;
-            }
-        }
-        .hamburger-menu{
-            overflow: hidden;
-            display:flex;
-            flex-direction: column;
-            justify-content: center;
-            height: 100vh;
-            width: 100vw;
-            position: fixed;
-            margin-left:-100vw;
-            background: linear-gradient(197.4deg, #2463AA 0%, #5AC2D8 100.71%);
-            top:0px;
-            left:0px;
-            transition: all 200ms ease-in-out;
-            img{
-                width: 150px;
-                margin: 0 auto 50px auto;
-            }
-            .close{
-                position: absolute;
-                width: 40px;
-                top:70px;
-                right: 60px;
-                cursor: pointer;
-                img{
-                    width: 100%;
-                }
-            }
-            h1{
-                    color:white;
-                    font-size:3em;
-                    margin: 0 auto 0 auto;
-                }
-            p{
-                margin-left:25%;
-                a{
-                    color:white;
-                }
-            }
-}
+      }
     }
-    
-}
-@media (max-width:480px){
-    nav{
-        height: 100px;
-        .hamburger{
-            width: 20px;
-            top: 40px;
-            right:40px;
+    ul {
+      display: none;
+    }
+    .hamburger {
+      display: inline;
+      position: absolute;
+      right: 60px;
+      top: 70px;
+      width: 40px;
+
+      cursor: pointer;
+      img {
+        width: 100%;
+      }
+    }
+    .hamburger-menu {
+      overflow: hidden;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      height: 100vh;
+      width: 100vw;
+      position: fixed;
+      margin-left: -100vw;
+      background: linear-gradient(197.4deg, #2463aa 0%, #5ac2d8 100.71%);
+      top: 0px;
+      left: 0px;
+      transition: all 200ms ease-in-out;
+      img {
+        width: 150px;
+        margin: 0 auto 50px auto;
+      }
+      .close {
+        position: absolute;
+        width: 40px;
+        top: 70px;
+        right: 60px;
+        cursor: pointer;
+        img {
+          width: 100%;
         }
-        .hamburger-menu{
-            .close{
-                width: 20px;
-            top: 40px;
-            right:40px;
-            }
-            h1{
-                font-size:2em;
-            }
-            p{
-                font-size:1em;
-                margin: 5% 0 0 25%;
-            }
+      }
+      h1 {
+        color: white;
+        font-size: 3em;
+        margin: 0 auto 0 auto;
+      }
+      p {
+        margin-left: 25%;
+        a {
+          color: white;
+        }
+      }
+    }
+  }
+}
+@media (max-width: 480px) {
+  nav {
+    height: 100px;
+    .hamburger {
+      width: 20px;
+      top: 45px;
+      right: 40px;
+    }
+    .hamburger-menu {
+      .close {
+        width: 20px;
+        top: 45px;
+        right: 40px;
+      }
+      h1 {
+        font-size: 2em;
+      }
+      p {
+        font-size: 1em;
+        margin: 5% 0 0 25%;
+      }
+    }
+  }
+}
+
+@media (max-width: 321px) {
+  nav {
+    .hamburger {
+      right: 5px;
+    }
+    .hamburger-menu{
+        .close{
+            right:5px;
         }
     }
-    
+  }
 }
 </style>
 
 <script>
 export default {
-    name:"NavBar",
-    methods:{
-        hmbMenu(){
-            if(document.querySelector(".hamburger-menu").style.marginLeft=="-100vw"){
-                //Si le menu est à gauche, le met au centre, bloque le scroll et crache la flèche
-                document.querySelector(".hamburger-menu").style.marginLeft="0"
-                document.querySelector("body").style.overflow="hidden"
-                document.querySelector(".fleche").style.display="none"
-            }
-            else{
-                document.querySelector("body").style.overflow="auto"
-                document.querySelector(".fleche").style.display="block"
-                document.querySelector(".hamburger-menu").style.marginLeft="-100vw"
-                window.scroll(0,-10000)
-            }
-        }
-        
-    }
-}
+  name: "NavBar",
+  methods: {
+    hmbMenu() {
+      if (
+        document.querySelector(".hamburger-menu").style.marginLeft == "-100vw"
+      ) {
+        //Si le menu est à gauche, le met au centre, bloque le scroll et crache la flèche
+        document.querySelector(".hamburger-menu").style.marginLeft = "0";
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".fleche").style.display = "none";
+      } else {
+        document.querySelector("body").style.overflow = "auto";
+        document.querySelector(".fleche").style.display = "block";
+        document.querySelector(".hamburger-menu").style.marginLeft = "-100vw";
+        window.scroll(0, -10000);
+      }
+    },
+  },
+};
 </script>
